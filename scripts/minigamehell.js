f613be5c68cf09b8f6d66d627191d1800c0f5a69
Yong Woon Jang <neostage@github.com>
2015-05-02 01:17:59 +0900

    commit : match test

--- scripts/minigamehell.js
+++ scripts/minigamehell.js
@@ -22,7 +22,7 @@ module.exports = function(robot) {
 
 
   robot.hear(
-    /게임시작/i, 
+    /회원가입/i, 
     function(res){
       if(robot.brain[""+res.envelope.user.id] == null) 
       {
@@ -39,10 +39,9 @@ module.exports = function(robot) {
   robot.hear(
     /나의정보/i, 
     function(res){
-      if(robot.brain[""+res.envelope.user.id] == null){
+      if(robot.brain[""+res.envelope.user.id] == null) {
         res.send("넌 가입을 하지 않았다.")
-      }
-      else{
+      } else {
         var next_upgrade_money = (100*(robot.brain[""+res.envelope.user.id].goza * robot.brain[""+res.envelope.user.id].goza)) - (50 * robot.brain[""+res.envelope.user.id].goza)
         res.send(res.envelope.user.name+"은(는) $"+robot.brain[""+res.envelope.user.id].money+" 만큼 돈이 있고 신분은 "+robot.brain[""+res.envelope.user.id].goza+" 단계이다.");
         res.send("다음 강화하는데 드는 비용: $ "+next_upgrade_money);
@@ -56,21 +55,27 @@ module.exports = function(robot) {
     function(res){
       var upgrade_money = (100*(robot.brain[""+res.envelope.user.id].goza * robot.brain[""+res.envelope.user.id].goza)) - (50 * robot.brain[""+res.envelope.user.id].goza)
 
-        if(robot.brain[""+res.envelope.user.id].money > upgrade_money){
-          robot.brain[""+res.envelope.user.id].money = robot.brain[""+res.envelope.user.id].money - upgrade_money
+      if(robot.brain[""+res.envelope.user.id].money > upgrade_money)
+      {
+        robot.brain[""+res.envelope.user.id].money = robot.brain[""+res.envelope.user.id].money - upgrade_money;
 
-          if(Math.random() <= Math.pow(0.9, robot.brain[""+res.envelope.user.id].goza-1)){
-            robot.brain[""+res.envelope.user.id].goza = robot.brain[""+res.envelope.user.id].goza + 1;
-            res.send(res.envelope.user.name+" 신분상승 성공! "+robot.brain[""+res.envelope.user.id].goza+" 단계가 되었다");
-          }
-          else {
-            robot.brain[""+res.envelope.user.id].goza=1;
-            res.send(res.envelope.user.name+" 신분상승 실패! 신분 초기화됨 병1신");
-          }
+        if(Math.random() <= Math.pow(0.9, robot.brain[""+res.envelope.user.id].goza-1)) {
+          robot.brain[""+res.envelope.user.id].goza = robot.brain[""+res.envelope.user.id].goza + 1;
+          res.send(res.envelope.user.name+" 신분상승 성공! "+robot.brain[""+res.envelope.user.id].goza+" 단계가 되었다");
+        } else {
+          robot.brain[""+res.envelope.user.id].goza=1;
+          res.send(res.envelope.user.name+" 신분상승 실패! 신분 초기화됨 병1신");
         }
-        else{
-          res.send("신분상승을 하려면 $"+upgrade_money+" 만큼의 돈이 필요하다");
-        }
+      } else {
+        res.send("신분상승을 하려면 $"+upgrade_money+" 만큼의 돈이 필요하다");
+      }
+    }
+  );
+
+  robot.hear(
+    /게임준비( 마피아)?/i,
+    function(res) {
+      res.send(res.match[0]+" "+res.match[1]);
     }
   );
 }
