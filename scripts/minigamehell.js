0b171857abbfba9326d2f8517a0dc38035164496
chi3236 <chi3236@gmail.com>
2015-05-01 22:20:24 +0900

    계정 생성까지 완료

--- scripts/minigamehell.js
+++ scripts/minigamehell.js
@@ -4,10 +4,18 @@
 
 module.exports = function(robot){
   robot.hear(/게임시작/i, function(res){
-    res.send("안녕 "+res.envelope.user.name+". 지금부터 게임을 시작한다");
-	//res.send("Repository : https://github.com/zeropage/zerobot");
-    //res.send("기능 추가 및 버그 수정은 github Repository에 Pull Request로 보내주시기 바랍니다.");
-    //res.send("※ Zerobot을 사용하기 위해서는 각 채널에 초대를 하시기 바랍니다.");
+    if(robot.brain[""+res.envelope.user.name] == null){
+    	res.send("안녕 "+res.envelope.user.name+". 지금부터 게임을 시작한다");
+    	robot.brain[""+res.envelope.user.id] = {id:res.envelope.user.id, money:0};
+	}
+	else{
+		res.send("넌 이미 가입되어 있다.");
+	}
+  });
+
+  robot.hear(/나의정보/i, function(res){
+  	res.send(res.envelope.user.name+"은 $"+robot.brain[""+res.envelope.user.id].money+" 만큼 돈이 있다")
   });
 }
+  
 
