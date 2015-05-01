44342206f9dd9e34693014bde182ecfbd7ced434
chi3236 <chi3236@gmail.com>
2015-05-01 23:05:25 +0900

    큰 틀을 만듦

--- scripts/minigamehell.js
+++ scripts/minigamehell.js
@@ -5,8 +5,8 @@
 module.exports = function(robot){
   robot.hear(/게임시작/i, function(res){
     if(robot.brain[""+res.envelope.user.id] == null){
-    	res.send("안녕 "+res.envelope.user.name+". 지금부터 게임을 시작한다");
-    	robot.brain[""+res.envelope.user.id] = {id:res.envelope.user.id, money:0};
+    	res.send("안녕 "+res.envelope.user.name+". 지금부터 게임을 시작한다")
+    	robot.brain[""+res.envelope.user.id] = {id:res.envelope.user.id, money:99999999, goza:1};
 	}
 	else{
 		res.send("넌 이미 가입되어 있다.");
@@ -20,6 +20,25 @@ module.exports = function(robot){
   	else{
   		res.send(res.envelope.user.name+"은 $"+robot.brain[""+res.envelope.user.id].money+" 만큼 돈이 있다")
     }
+  });
+
+  robot.hear(/신분상승/i, function(res){
+  	var upgrade_money = (robot.brain[""+res.envelope.user.id].goza * robot.brain[""+res.envelope.user.id].goza) - (50 * robot.brain[""+res.envelope.user.id].goza)
+
+  		if(robot.brain[""+res.envelope.user.id].money > upgrade_money){
+  			robot.brain[""+res.envelope.user.id].money = robot.brain[""+res.envelope.user.id].money - upgrade_money
+
+  			if(Math.random() < Math.pow(0.9, robot.brain[""+res.envelope.user.id].goza)-1){
+  				res.send("신분상승 성공!");
+  			}
+  			else {
+  				robot.brain[""+res.envelope.user.id].goza=1;
+  				res.send("신분상승 실패! 신분 초기화됨 병1신");
+  			}
+  		}
+  		else{
+  			res.send("신분상승을 하려면 $"+upgrade_money+" 만큼의 돈이 필요하다");
+  		}
   });
 }
   
